543323eb8eac835fe9e2f22eca5c21611890a01e
Manuel Florido <manuel.florido@sygel.es>
2024-07-11 13:26:14 +0200

    [T-5944][ADD] First commit

--- website_signup_additional_fields/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2023 Manuel Regidor <manuelregidor@sygel.es>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import res_config_settings
-from . import website
-from . import res_users
